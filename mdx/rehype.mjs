27631b618ea6f73045e46555a3c028367dd68a3e
Jesse Wierzbinski <contact@cpluspatch.com>
2024-07-22 12:12:40 +0200

    chore: :arrow_up: Upgrade all dependencies except shiki

--- mdx/rehype.mjs
+++ mdx/rehype.mjs
@@ -8,7 +8,7 @@ import { visit } from "unist-util-visit";
 function rehypeParseCodeBlocks() {
     return (tree) => {
         // biome-ignore lint/style/useNamingConvention: <explanation>
-        visit(tree, "element", (node, _, parentNode) => {
+        visit(tree, "element", (node, _nodeIndex, parentNode) => {
             if (node.tagName === "code" && node.properties.className) {
                 parentNode.properties.language =
                     node.properties.className[0]?.replace(/^language-/, "");
