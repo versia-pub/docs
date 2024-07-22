4d08b7e8b0695c1fb7ad07216357e89a4bf17382
Jesse Wierzbinski <contact@cpluspatch.com>
2024-07-22 13:41:51 +0200

    feat: :building_construction: Export as static site

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+    output: "export"
 };
 
 export default withSearch(withMDX(nextConfig));
