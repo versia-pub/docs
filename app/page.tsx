141f90092917bd751930bd0b47d1bed56f46362b
Jesse Wierzbinski <contact@cpluspatch.com>
2024-08-24 14:38:13 +0200

    fix: :truck: Point links to new address for Versia Server

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ const Page: FC = () => {
                     Versia Server, which uses Versia for federation. This
                     community could include you! Check out our{" "}
                     <a
-                        href="https://github.com/lysand-org/lysand"
+                        href="https://github.com/lysand-org/server"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
