064a4aa8289a831127c05dacfce49c94f9dcc056
Jesse Wierzbinski <contact@cpluspatch.com>
2025-09-21 17:07:59 +0200

    fix: :lipstick: Remove navbar and footer on print

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -129,7 +129,7 @@ function SmallPrint() {
 
 export function Footer() {
     return (
-        <footer className="mx-auto w-full max-w-2xl space-y-10 pb-16 lg:max-w-5xl">
+        <footer className="mx-auto w-full max-w-2xl space-y-10 pb-16 lg:max-w-5xl print:hidden">
             <PageNavigation />
             <SmallPrint />
         </footer>
